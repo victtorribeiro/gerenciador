447045503c02c9c9c056f562d5d69172a54bd0eb
Victor Matheus Silva Ribeiro <89957280+victtorribeiro@users.noreply.github.com>
2022-04-30 01:42:58 -0300

    teste

--- app/Models/User.js
+++ app/Models/User.js
@@ -9,7 +9,6 @@ const Hash = use('Hash')
 class User extends Model {
   static boot () {
     super.boot()
-
     /**
      * A hook to hash the user password before saving
      * it to the database.
